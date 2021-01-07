62feb855ef46a55c7872382b153a35864dc1a0be
M1-135-HaiderFaisal <76619433+M1-135-HaiderFaisal@users.noreply.github.com>
2021-01-07 20:48:04 +0300

    تم الحل

--- myscript.js
+++ myscript.js
@@ -5,10 +5,46 @@ let board = [
 ]
 
 
+let play ="o"
 
-function press(cellNO) {
-
+function press(x,y) {
     // Do stuff
-    console.log(cellNO)
-    
+    console.log(x,y)
+   
+   if (play =="o"){
+       play="x"
+   }else{
+       play="o"
+   }
+   board[x][y]=play
+   let cell = document.getElementById(`${x}${y}`);
+   cell.innerText =play;
+
+
+
+   if(play== board [0][0] && board[0][1]==play && board[0][2]==play){
+   console.log(`nice${play}`)}
+   
+   
+   else if(play==board [1][0] && board[1][1]==play &&board [1][2]==play){
+       console.log(`nice${play}`)
+   }
+   else if(play==board [2][0] && board[2][1]==play &&board [2][2]==play){
+       console.log(`nice${play}`)
+   }
+   else if(play==board [0][0] && board[1][0]==play && board[2][0]==play){
+       console.log(`nice${play}`)
+   }
+   else if(play==board [0][1]&& board[1][1]==play && board[2][1]==play){
+       console.log(`nice${play}`)
+   }
+   else if(play==board [0][2] && board[1][2]==play &&board [2][2]==play){
+       console.log(`nice${play}`)
+   }
+   else if(play==board [0][0] && board[1][1]==play && board[2][2]==play){
+       console.log(`nice${play}`)
+   }
+   else if (play==board [0][2] && board[1][1]==play &&board [2][0]==play){
+       console.log(`nice${play}`)
+   }
 }
